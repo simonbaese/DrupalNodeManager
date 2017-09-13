1ef6ed04a412d089455c9c97c71769b9828c969d
Simon Boy <simon@youvo.org>
2017-09-13 13:23:55 -0400

    Refactor js behavior file

--- js/nodemanager.js
+++ js/nodemanager.js
@@ -2,79 +2,89 @@
 
 (function ($) {
 
-    $.fn.customAttach = function (nid, nodeview, rotate) {
-        if ($('tr#content-row-' + nid).is(':visible')) {
-            $('#nm-inspect-id-' + nid).removeClass('rotate');
-            $('tr#content-row-' + nid).insertAfter($('tr#nm-hidden-content'));
-            $('tr#content-row-' + nid + ' td').empty();
-        } else {
-            $('tr.content-row').insertAfter($('tr#nm-hidden-content'));
-            $('span.rotate').removeClass('rotate');
-            $('tr.content-row td').empty();
-            $('tr#content-row-' + nid + ' td').append(nodeview);
-            if ($('div#row-options-' + nid).is(':visible')) {
-                $('tr#content-row-' + nid).insertAfter($('tr#row-' + nid).next()).hide();
-            } else {
-                $('tr#content-row-' + nid).insertAfter($('tr#row-' + nid));
-                $('tr.row-options').insertAfter($('tr#nm-hidden-content'));
-            }
-            $('tr#content-row-' + nid).find('td').wrapInner('<div style="display: none;" />').parent().find('td > div').slideDown(500, function () {
-                var $set = $(this);
-                $set.replaceWith($set.contents());
-            });
-            if (rotate) $('#nm-inspect-id-' + nid).addClass('rotate');
-        }
-    };
+  $.fn.customAttach = function (nid, nodeview, rotate) {
 
-    Drupal.ajax.prototype.commands.nodeManagerReattach = function (ajax, response, status) {
-        Drupal.attachBehaviors();
-    };
+    var contentRow = $('tr#content-row-' + nid);
+    var hiddenContent = $('tr#nm-hidden-content');
 
-    Drupal.behaviors.nodeManager = {
-        attach: function (context) {
-
-            $('.nm-edit').once().on('click', function () {
-                var nid = $(this).data('nid');
-                if ($('tr#edit-row-' + nid).is(':visible')) {
-                    $('tr#edit-row-' + nid).insertAfter($('tr#nm-hidden-content'));
-                } else {
-                    hideContent();
-                    $('tr.content-row:not(#content-row-' + nid + ')').insertAfter($('tr#nm-hidden-content'));
-                    $('tr.row-options').insertAfter($('tr#nm-hidden-content'));
-                    $('tr#edit-row-' + nid).insertAfter($('tr#row-' + nid));
-                }
-            });
-
-            $('.nm-inspect').once().on('click', function () {
-                var nid = $(this).data('nid');
-                $('input[name=nid]').val(nid);
-                $('input[type=submit]#edit-inspect-submit').click();
-            });
-
-            // Hide everything on body click
-            $('body').once().on('click', function (event) {
-                if ($(event.target).closest('#nm-table').length == 0) {
-                    hideContent();
-                    $('tr.row-options').insertAfter($('tr#nm-hidden-content'));
-                }
-            });
-
-            $(document).ready(function () {
-                // Move form elements into table.
-                // Move buttons for options into button row of table.
-                $('.js-place-btn-row').each(function () {
-                    var nid = $(this).data('nid');
-                    $('div#row-buttons-' + nid).prepend(this);
-                });
-
-            });
-
-            function hideContent() {
-                $('tr.content-row').insertAfter($('tr#nm-hidden-content'));
-                $('span.rotate').removeClass('rotate');
-                $('tr.content-row td').empty();
-            }
-        }
+    if (contentRow.is(':visible')) {
+      $('#nm-inspect-id-' + nid).removeClass('rotate');
+      contentRow.insertAfter(hiddenContent);
+      $('tr#content-row-' + nid + ' td').empty();
+    } else {
+      $('tr.content-row').insertAfter(hiddenContent);
+      $('span.rotate').removeClass('rotate');
+      $('tr.content-row td').empty();
+      $('tr#content-row-' + nid + ' td').append(nodeview);
+      if ($('div#row-options-' + nid).is(':visible')) {
+        contentRow.insertAfter($('tr#row-' + nid).next()).hide();
+      } else {
+        contentRow.insertAfter($('tr#row-' + nid));
+        $('tr.row-options').insertAfter(hiddenContent);
+      }
+      contentRow.find('td').wrapInner('<div style="display: none;" />').parent().find('td > div').slideDown(500, function () {
+        var $set = $(this);
+        $set.replaceWith($set.contents());
+      });
+      if (rotate) $('#nm-inspect-id-' + nid).addClass('rotate');
     }
+  };
+
+  Drupal.Ajax.prototype.commands.nodeManagerReattach = function (ajax, response, status) {
+    Drupal.attachBehaviors();
+  };
+
+  Drupal.behaviors.nodeManager = {
+    attach: function (context) {
+
+      $('.nm-edit').once().on('click', function () {
+
+        var nid = $(this).data('nid');
+        var hiddenContent = $('tr#nm-hidden-content');
+        var editOptionRow = $('tr#edit-row-' + nid);
+
+        if (editOptionRow.is(':visible')) {
+          editOptionRow.insertAfter(hiddenContent);
+        } else {
+          hideContent();
+          $('tr.content-row:not(#content-row-' + nid + ')').insertAfter(hiddenContent);
+          $('tr.row-options').insertAfter(hiddenContent);
+          editOptionRow.insertAfter($('tr#row-' + nid));
+        }
+      });
+
+      $('.nm-inspect').once().on('click', function () {
+
+        var nid = $(this).data('nid');
+
+        $('input[name=nid]').val(nid);
+        $('input[type=submit]#edit-inspect-submit').click();
+      });
+
+      // Hide everything on body click
+      $('body').once().on('click', function (event) {
+        if ($(event.target).closest('#nm-table').length === 0) {
+          hideContent();
+          $('tr.row-options').insertAfter($('tr#nm-hidden-content'));
+        }
+      });
+
+      $(document).ready(function () {
+        // Move form elements into table.
+        // Move buttons for options into button row of table.
+        $('.js-place-btn-row').each(function () {
+          var nid = $(this).data('nid');
+          $('div#row-buttons-' + nid).prepend(this);
+        });
+
+      });
+
+      function hideContent() {
+        $('tr.content-row').insertAfter($('tr#nm-hidden-content'));
+        $('span.rotate').removeClass('rotate');
+        $('tr.content-row td').empty();
+      }
+    }
+  }
 
 }(jQuery));
